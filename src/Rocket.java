97ea16a4b2242328f8dc94086ca9b2c26c255536
Jakub Cichacki <kuba.cichacki@o2.pl>
2020-03-26 12:43:18 +0100

    Initial commit - project sceleton

---
+++ src/Rocket.java
@@ -0,0 +1,13 @@
+public class Rocket implements SpaceShip {
+
+    @Override
+    public boolean launch() {
+        return true;
+    }
+
+    @Override
+    public boolean land() {
+        return true;
+    }
+
+}
